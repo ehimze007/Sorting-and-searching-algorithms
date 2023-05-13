d75234620816ec71b8a3c6fae0b3fd04b4c4c22d
ehimze007 <ehimze007@gmail.com>
2023-05-13 16:59:15 +0100

    update to the Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Sorting-and-searching-algorithms
+
+- Loop through the array and compare each element to its left.
+- The outer loop starts at the second element and goes to the end of the array.
+- The inner loop starts at the current element and goes backwards to the beginning of the array, comparing each element to the current value.
+- If the current value is less than the compared value, we swap their positions.
+- Finally, we return the sorted array.
--- insertionSort.js
+++ insertionSort.js
@@ -1,25 +1,10 @@
-//Take a random array - [5,3,9,7,6,8]
-//Sort each element from least to highest
-
-function insertionSort(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 1; j <= array.length; j++) {
-      if (array[0] < array[j]) {
-        array[i - 1] = array[j];
-      }
-    }
-  }
-}
-
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    let currentVal = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > currentVal) {
+    let currentValue = arr[i];
+    for (let j = i - 1; j >= 0 && arr[j] > currentValue; j--) {
       arr[j + 1] = arr[j];
-      j--;
+      arr[j] = currentValue;
     }
-    arr[j + 1] = currentVal;
   }
   return arr;
 }
